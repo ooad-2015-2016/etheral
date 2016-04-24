9e0b4aa93ecbc26898608f62ed3727f5e18978d0
enafly <emuratspah1@etf.unsa.ba>
2016-04-25 00:11:24 +0200

    Zaposlenik

---
+++ ProjektniZadatak4ethereal/KinoProjekat/KinoProjekat/Kino/Models/Zaposlenik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoProjekat.Kino.Models
+{
+    enum TipZaposlenika { RadnikNaSalteru, AdministratorSistema};
+    class Zaposlenik
+    {
+        string ime, prezime;
+        TipZaposlenika uloga;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ZaposenikId { get; set; }
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public TipZaposlenika uloga { get; set; }
+
+    }
+}
---
+++ ProjektniZadatak4ethereal/KinoProjekat/KinoProjekat/Kino/Models/ZaposlenikDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Sqlite;
+using System.IO;
+using Windows.Storage;
+
+namespace KinoProjekat.Kino.Models
+{
+    class ZaposlenikDbContext : DbContext
+    {
+        public DbSet<Zaposlenik> SviZaposlenici { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dataBaseFilePath = "Ooadbaza.db";
+            try
+            {
+                dataBaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, dataBaseFilePath);
+            }
+            catch (InvalidOperationException) { }
+
+            optionsBuilder.UseSqlite($"Data source={dataBaseFilePath}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //
+        }
+    }
+}
