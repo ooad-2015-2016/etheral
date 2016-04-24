053c0232d97382a4fe7d7d1b4473627fb908b299
enafly <emuratspah1@etf.unsa.ba>
2016-04-25 00:17:22 +0200

    Zaposlenik

--- ProjektniZadatak4ethereal/KinoProjekat/KinoProjekat/Kino/Models/Zaposlenik.cs
+++ ProjektniZadatak4ethereal/KinoProjekat/KinoProjekat/Kino/Models/Zaposlenik.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KinoProjekat.Kino.Models
 {
     enum TipZaposlenika { RadnikNaSalteru, AdministratorSistema};
     class Zaposlenik
     {
-        string ime, prezime;
-        TipZaposlenika uloga;
+        string imeZ, prezimeZ;
+        TipZaposlenika ulogaZ;
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ZaposenikId { get; set; }
+        public int zaposenikId { get; set; }
         public string ime { get; set; }
         public string prezime { get; set; }
         public TipZaposlenika uloga { get; set; }
